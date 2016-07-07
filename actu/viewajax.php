79b5bea0b3c5437c86cc128ecaf793d2db8b92d7
erwansimplon <guilleterwan@aol.com>
2016-07-07 23:43:54 +0200

    Update viewajax.php

--- actu/viewajax.php
+++ actu/viewajax.php
@@ -15,9 +15,13 @@ $comment=$r['comments'];
 
 
 <div class="comment_ui" >
-<div class="comment_text">
-<div  class="comment_actual_text"><img id="profil_img" src="../auth-photos/'.$id.$image.'images.jpg" /><div id="comment_post"><?php echo $comment; ?></div></div>
-</div>
+		<div class="comment_text">
+				<div  class="comment_actual_text">
+						<img id="profil_img" src="../auth-photos/'.$id.$image.'images.jpg" />
+						<div id="comment_post"><?php echo $comment; ?>
+						</div>
+				</div>
+		</div>
 </div>
 
 
@@ -26,12 +30,13 @@ $comment=$r['comments'];
 }
 ?>
 <div class="add_comment">
-<div>
-<img src="../auth-photos/'.$id.$image.'images.jpg" id="profil_img" />
-<form action="../actu/savecomment.php" method="post">
-<input name="mesgid" type="hidden" value="<?php echo $id ?>" />
-<input name="mcomment" type="text" placeholder="..." id="largeur_input_comment" />
-<input id="Envoyer" name="" type="submit" value="Envoyer" />
-</form>
-</div>
+		<div>
+				<img src="../auth-photos/'.$id.$image.'images.jpg" id="profil_img" />
+				
+				<form action="../actu/savecomment.php" method="post">
+						<input name="mesgid" type="hidden" value="<?php echo $id ?>" />
+						<input name="mcomment" type="text" placeholder="..." id="largeur_input_comment" />
+						<input id="Envoyer" name="" type="submit" value="Envoyer" />
+				</form>
+		</div>
 </div>
