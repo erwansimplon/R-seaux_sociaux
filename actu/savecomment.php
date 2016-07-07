f26cbd09cbd82c51bdd4a80ea9430fa209203f93
erwansimplon <guilleterwan@aol.com>
2016-07-07 16:06:20 +0200

    Update savecomment.php

--- actu/savecomment.php
+++ actu/savecomment.php
@@ -1,8 +1,13 @@
 <?php
-include("../bdd/db.php");
+// va chercher le fichier connexion 
+include("../bdd/connexion_bdd.php");
+// capture le champs envoyer 
 $mcomment=$_POST['mcomment'];
+// capture le id du message ou est écrit le commentaire
 $mesgid=$_POST['mesgid'];
+// écrit les donné dans la bdd  
 mysql_query("INSERT INTO comments (comments, msg_id_fk)
 VALUES ('$mcomment','$mesgid')");
+//redirige vers la page initial
 header("location: ../general/user.php");
 ?>
