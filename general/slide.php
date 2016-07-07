98048284d2328bd4afdcbd63ace2c25aa277f7fc
erwansimplon <guilleterwan@aol.com>
2016-07-08 00:06:28 +0200

    Update slide.php

--- general/slide.php
+++ general/slide.php
@@ -1,18 +1,22 @@
-<HTML>
-<HEAD>
+<html>
+<head>
   <meta http-equiv="content-Type" content="text/html; charset=uf-8">
   <title>Slide</title>
   <script language="JavaScript" src="slide.js"></script>
   <link rel="stylesheet" type="text/css" href="slide.css">
-</HEAD>
+</head>
 
-<BODY onLoad="preload(<?php'../auth-photos/profil/'$id.$image.$i.'.jpg'; ?>)">
+<body onLoad="preload(<?php'../auth-photos/'.$id.$image.'images.jpg'; ?>)">
   <h2>Manuel</h2>
-  <img src="../auth-photos/profil/'$id.$image.$i.'.jpg" id="Puzzle" name="Puzzle">
-  <a href="javascript:arret()"><img src=<?php'../auth-photos/profil/'$id.$image.$i.'.jpg'?> id="Slide" name="Slide" alt="clic pour arreter"></a><br/>
-  <a href="javascript:chgSlide(-1)"><b> << </b></a> &nbsp; <script language="JavaScript">autoSlide('Slide')</script> &nbsp; <a href="javascript:chgSlide(1)"><b> >> </b></a>
+  <img src="<?php'../auth-photos/profil/'.$id.$image.'images.jpg'?> id="Puzzle" name="Puzzle">
+  <a href="javascript:arret()"><img src=<?php'../auth-photos/profil/'.$id.$image.'images.jpg'?> 
+  id="Slide" name="Slide" alt="clic pour arreter"></a><br/>
+  
+  <a href="javascript:chgSlide(-1)"><b> << </b></a> &nbsp; 
+  <script language="JavaScript">autoSlide('Slide')</script> &nbsp; 
+  <a href="javascript:chgSlide(1)"><b> >> </b></a>
 
 
 
-</BODY>
-</HTML>
+</body>
+</html>
