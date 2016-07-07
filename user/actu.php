1fe2062e6e17f939fa55b17bad62b95c91b33090
erwansimplon <guilleterwan@aol.com>
2016-07-07 23:52:37 +0200

    Update actu.php

--- user/actu.php
+++ user/actu.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//FR"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-				<link rel="stylesheet" href="../css/actu-style.css" type="text/css" media="screen"/>
-				<!-- appel de ajax -->
-				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
-				<script type="text/javascript">
-							$(function()
-							{
-							$(".view_comments").click(function()
-							{
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="../css/actu-style.css" type="text/css" media="screen"/>
+		<!-- appel de ajax -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.0/jquery.min.js"></script>
+		<script type="text/javascript">
+			$(function()
+			{
+			$(".view_comments").click(function()
+			{
 
-							var ID = $(this).attr("id");
+			var ID = $(this).attr("id");
 
-							$.ajax({
-							type: "POST",
-							url: "../user/viewajax.php",
-							data: "msg_id="+ ID,
-							cache: false,
-							success: function(html){
-							$("#view_comments"+ID).prepend(html);
-							$("#view"+ID).remove();
-							$("#two_comments"+ID).remove();
-							}
-							});
+			$.ajax({
+			type: "POST",
+			url: "../user/viewajax.php",
+			data: "msg_id="+ ID,
+			cache: false,
+			success: function(html){
+			$("#view_comments"+ID).prepend(html);
+			$("#view"+ID).remove();
+			$("#two_comments"+ID).remove();
+			}
+			});
 
-							return false;
-							});
-							});
-				</script>
-				<!-- du script ajax -->
-			</head>
+			return false;
+			});
+			});
+		</script>
+		<!-- du script ajax -->
+	</head>
 
-<body>
+	<body>
 
-<ol>
-		<li>
+		<ol>
+			<li>
 			<!-- formulaire qui permet a l'utilisateur de poster les message -->
 				<form action="../user/savemessage.php" method="post">
-						<textarea class="editbox" cols="23" rows="3" name="message"></textarea><br />
-						<input id="POST" name="POST" type="submit" value="POST" />
+					<textarea class="editbox" cols="23" rows="3" name="message"></textarea><br />
+					<input id="POST" name="POST" type="submit" value="POST" />
 				</form>
-		</li>
+			</li>
 
 <?php
 // va cherche le fichier connection 
@@ -57,15 +57,17 @@ $msgcontent=$messagecount['message'];
 
 <li class="comment_envoyer">
 
-		<div class="comment_squelette">
-				<h3 class="Message" >
-					<?php
-					//Si le membre possède une image, on l'affiche
-					if (file_exists('../auth-photos/'.$id.$image.'images.jpg')){
-					echo '<img class="avatar" style="float:left;" alt="avatar" src="../auth-photos/'.$id.$image.'images.jpg"/>';
-					}
-					?><strong><?php echo $pseudo; ?></strong>
-						<p><?php echo $msgcontent; ?></p>
+	<div class="comment_squelette">
+		<h3 class="Message" >
+			<?php
+			//Si le membre possède une image, on l'affiche
+			if (file_exists('../auth-photos/'.$id.$image.'images.jpg')){
+			echo '<img class="avatar" style="float:left;" alt="avatar" 
+			src="../auth-photos/'.$id.$image.'images.jpg"/>';
+			}
+			?>
+			<strong><?php echo $pseudo; ?></strong>
+			<p><?php echo $msgcontent; ?></p>
 
 		<div id="message_conteneur">
 
@@ -80,7 +82,10 @@ $second_count=$comment_count-2;
 ?>
 
 		<div class="comment_ui" id="view<?php echo $id; ?>">
-				<div><a href="#" class="view_comments" id="<?php echo $id; ?>">Voir les <?php echo $comment_count; ?> autres commentaires</a></div>
+			<div>
+				<a href="#" class="view_comments" id="<?php echo $id; ?>">
+					Voir les <?php echo $comment_count; ?> autres commentaires</a>
+			</div>
 		</div>
 
 <?php
@@ -106,18 +111,25 @@ $comment=$rowsmall['comments'];
 		<div class="comment_ui">
 
 		<div class="comment_text">
-				<div  class="comment_actual_text">
-					<?php
-				            //Si le membre possède une image, on l'affiche
-				            if (file_exists('../auth-photos/'.$id.$image.'images.jpg')){
-				            echo '<img class="avatar" style="float:left;" alt="avatar" src="../auth-photos/'.$id.$image.'images.jpg"/>';
-				            }
-				            ?><div id="comment_post"><?php echo $comment; ?></div></div>
+			<div  class="comment_actual_text">
+				<?php
+				//Si le membre possède une image, on l'affiche
+				if (file_exists('../auth-photos/'.$id.$image.'images.jpg')){
+				echo '<img class="avatar" style="float:left;" alt="avatar" 
+				src="../auth-photos/'.$id.$image.'images.jpg"/>';
+				}
+				?>
+				<div id="comment_post">
+					<?php echo $comment; ?>
+				</div>
+			</div>
 		</div>
 
-</div>
+	</div>
 
-<?php } ?>
+<?php 
+}
+?>
 		<div class="add_comment">
 			<div>
 				<?php
